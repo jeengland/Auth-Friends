9f33e00806a5a4a15a32f99e82a9339df60b25a3
unknown <jacob.e.england@gmail.com>
2020-03-24 16:54:22 -0500

    Added axios call to pull data from API

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendList = (props) => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/api/friends')
+            .then((response) => setData(response.data))
+            .catch((error) => console.error(`${error.response.status}: ${error.response.statusText}`));
+    }, [])
     return (
         <h1>Hello from Friend List</h1>
     )
