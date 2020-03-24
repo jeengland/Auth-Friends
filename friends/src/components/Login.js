721c0d02cecbd14bc5933a99a96d06109fc37719
unknown <jacob.e.england@gmail.com>
2020-03-24 15:06:47 -0500

    Added better error handling to login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -4,6 +4,7 @@ import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 const Login = (props) => {
     const [login, setLogin] = useState({username: '', password: ''});
+    const [error, setError] = useState('');
     const handleChange = (event) => {
         setLogin({
             ...login,
@@ -15,9 +16,11 @@ const Login = (props) => {
         axiosWithAuth()
             .post('/api/login', login)
             .then((response) => {
-                localStorage.setItem('token', response.data.payload)
+                localStorage.setItem('token', response.data.payload);
+                setError('');
             })
-            .catch((error) => console.error(error.response));
+            .catch((error) => setError(error.response.data.error));
+        setLogin({username: '', password: ''});
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -30,6 +33,7 @@ const Login = (props) => {
                 <input type='password' id='password' name='password' value={login.password} onChange={handleChange}/>
             </div>
             <input type='submit' />
+            {error ? <p className='error'>{error}</p> : undefined }
         </form>
     )
 }
