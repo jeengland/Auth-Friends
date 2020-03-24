d01d9fe0f1b47110459fe1909d4896e0c57a5751
unknown <jacob.e.england@gmail.com>
2020-03-24 15:01:35 -0500

    Added axiosWithAuth and added submit handler to login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
 const Login = (props) => {
     const [login, setLogin] = useState({username: '', password: ''});
     const handleChange = (event) => {
@@ -8,8 +10,17 @@ const Login = (props) => {
             [event.target.name]: event.target.value,
         })
     }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axiosWithAuth()
+            .post('/api/login', login)
+            .then((response) => {
+                localStorage.setItem('token', response.data.payload)
+            })
+            .catch((error) => console.error(error.response));
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='formRow'>
                 <label htmlFor='username'>Username:</label>
                 <input type='text' id='username' name='username' value={login.username} onChange={handleChange}/>
@@ -18,6 +29,7 @@ const Login = (props) => {
                 <label htmlFor='password'>Password:</label>
                 <input type='password' id='password' name='password' value={login.password} onChange={handleChange}/>
             </div>
+            <input type='submit' />
         </form>
     )
 }
