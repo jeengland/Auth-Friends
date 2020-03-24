c07cf4628b50e535bfc490ddd80584d42f740023
unknown <jacob.e.england@gmail.com>
2020-03-24 16:29:24 -0500

    Set up router and PrivateRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
 
+import PrivateRoute from './components/PrivateRoute';
+import FriendList from './components/FriendList';
+
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <div className='container'>
+        <Switch>
+          <PrivateRoute exact path='/friend_list' component={FriendList} />
+          <Route path='/login' component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FriendList = (props) => {
+    return (
+        <h1>Hello from Friend List</h1>
+    )
+}
+
+export default FriendList;
